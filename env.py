b239db6033db005a58e8b9a5c27d6c3540a096c1
AndreaPolac <55705222+AndreaPolac@users.noreply.github.com>
2021-10-31 17:28:40 +0100

    Update env.py

--- env.py
+++ env.py
@@ -1,31 +1,62 @@
-class GameEnvironment(object):
-    def __init__(self, column_player, T=5, R=3, P=1, S=0):
-        self.action_space = [0, 1] # 0:cooperate 1:defect
-        self.observation_space = {0 : np.array([0, 0], dtype=np.uint8), # CC
-                                  1 : np.array([0, 1], dtype=np.uint8), # CD
-                                  2 : np.array([1, 0], dtype=np.uint8), # DC
-                                  3 : np.array([1, 1], dtype=np.uint8)} # DD
+import numpy as np
+import gym
+from gym import spaces
+
+# symmetric 2 player Markov game
+# state: 2-dim array representing previous couple of choosen actions
+class MarkovGame(gym.Env):
+    def __init__(self, row_player, col_player, payoff_table, episode_len):
+        super(MarkovGame, self).__init__()
+        self.row_player = row_player
+        self.col_player = col_player
+        self.payoff_table = payoff_table
+        self.episode_len = episode_len
+        self.step_cntr = 0
+        self.n_actions = payoff_table.shape[0]
+        # All possible couple of actions i.e. states
+        self.possible_states = [np.array([i, j], dtype=np.uint8) 
+                                        for i in range(n_actions) 
+                                        for j in range(n_actions)]
+        self.n_states = len(self.possible_states)
         
-        self.payoff_table = np.array([[(R,R), (S,T)],
-                                      [(T,S), (P,P)]])
+        # Define observation space 
+        self.observation_space = spaces.Box(low=0, high=n_actions-1,
+                                            shape=(2,), dtype=np.uint8)
+        # Define an action space
+        self.action_space = spaces.Discrete(n_actions)
         
-        self.column_player = column_player
+        self.index_to_state = {i : self.possible_states[i] 
+                                   for i in range(self.n_states)}
         
+    def reset(self):
+        row_init_action = self.row_player.initial_action
+        col_init_action = self.col_player.initial_action
         
-    def reset(self, row_player):
-        self.state = np.array([row_player.initial_action, 
-                               self.column_player.initial_action])
-        self.column_state = np.array([self.state[1], self.state[0]])
+        self.row_player_state = np.array([row_init_action, col_init_action], 
+                                         dtype=np.uint8)
+        self.col_player_state = np.array([col_init_action, row_init_action], 
+                                         dtype=np.uint8)
+        initial_state = self.row_player_state
         
-        return self.state
-    
+        self.step_cntr = 0
+        
+        return initial_state
     
     def step(self, action):
-        column_action = self.column_player.choose_action(self.column_state)
+        # column player action
+        col_action = self.col_player.choose_action(self.col_player_state)
         
-        reward = self.payoff_table[action][column_action][0]
+        # row player reward according to payoff table
+        reward = self.payoff_table[action][col_action][0]
         
-        self.state = np.array([action, column_action], dtype=np.uint8)
-        self.column_state = np.array([self.state[1], self.state[0]])
+        # update states of the players
+        self.row_player_state = np.array([action, col_action], dtype=np.uint8)
+        self.col_player_state = np.array([col_action, action], dtype=np.uint8)
+        state = self.row_player_state
         
-        return reward, self.state
+        # check if terminal state is reached
+        done = self.step_cntr == self.episode_len
+        info = {}
+        self.step_cntr += 1
+        
+        return state, reward, done, info
